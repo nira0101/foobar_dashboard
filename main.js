458628a7755d0f4fb9f90770d7a09a8ebca58366
Jesus lopez <jesu0035@stud.kea.dk>
2021-05-22 02:09:40 +0200

    grids

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function displayEmployeeData(){
   let counter = 0;
   selector.forEach( () => {
       counter++
-      document.querySelector(`#worker_status_app .employee_${counter} h5`).innerHTML = globalData.bartenders[(counter-1)].name;
+      document.querySelector(`#worker_status_app .employee_${counter} h5`).innerHTML = `Name:${globalData.bartenders[(counter-1)].name}`;
       document.querySelector(`#worker_status_app .employee_${counter} h3`).innerHTML = `Customer: ${globalData.bartenders[(counter-1)].servingCustomer}`;
       document.querySelector(`#worker_status_app .employee_${counter} h4`).innerHTML = globalData.bartenders[(counter-1)].statusDetail;
   })
